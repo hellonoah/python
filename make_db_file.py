4444109f8121d2107b2dba6611f76150cee09304
rjarmand <rjarmand@yahoo.com>
2014-03-25 14:51:00 -0700

    add cgi directory
    Add cgi directory and files.

--- make_db_file.py
+++ make_db_file.py
@@ -15,11 +15,11 @@ def storeDbase(db, dbfilename=dbfilename):
     "formatted dump of database to flat file"
 dbfile = open(dbfilename, 'w')
 for key in db:
-	print(key, file=dbfile)
+  print(key, file=dbfile)
 for (name, value) in db[key].items():
-            print(name + RECSEP + repr(value), file=dbfile)
-	print(ENDREC, file=dbfile)
-	print(ENDDB, file=dbfile)
+  print(name + RECSEP + repr(value), file=dbfile)
+  print(ENDREC, file=dbfile)
+  print(ENDDB, file=dbfile)
 	dbfile.close()
 
 def loadDbase(dbfilename=dbfilename):
