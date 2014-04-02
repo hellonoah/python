94cec357df1a99f755998a34e1a74e89717d2a5b
rjarmand <rjarmand@yahoo.com>
2014-04-01 17:15:36 -0700

    add files for python repository

--- gui1.py
+++ gui1.py
@@ -1,4 +1,4 @@
-from Tkinter import Label                       # get a widget object
+from tkinter import Label                       # get a widget object
 widget = Label(None, text='Hello GUI world!')   # make one
 widget.pack() 					# arrange it
 widget.mainloop() 				# start event loop
